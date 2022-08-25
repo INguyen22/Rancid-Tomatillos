cfc0ada7707f9e1c86bc7ffd83c781fa8ebb384c
Ivy Nguyen <animeweirdo24@gmail.com>
2022-08-25 16:04:12 -0600

    fetch api data and use that to render the movie posters and details

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -12,16 +12,23 @@ import "./MovieCard.css"
 //     )
 // }
 
+//questions for kim: how do i use componenet did update
+//refactor functions to not have full fetch function inside of them
+//how to get back all the movies witrhout a re-fetch unless i have too
+
 class MovieCard extends React.Component {
-    constructor({id, title, description, rating, posterImage , backdropImage, showMovieDetails, goBack}) {
+    constructor({id, title, posterImage, showMovieDetails, goBack}) {
         super()
         this.state = {
             posterImage: posterImage,
-            backdropImage: backdropImage,
+            backdropImage: '',
             id: id,
             title: title,
-            description: description,
-            rating: rating,
+            releaseDate: '',
+            overview: '',
+            genre: [],
+            runtime: 0,
+            rating: 0,
             showMovieDetails: showMovieDetails,
             goBack: goBack,
             wasClicked: false
@@ -30,26 +37,34 @@ class MovieCard extends React.Component {
     }
     handleClick = () => {
         this.state.showMovieDetails(this.state.id)
-        this.setState(prevState => {
-            return {
-                wasClicked: !prevState.wasClicked
-            }
+        fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.state.id}`)
+        .then(response => response.json())
+        .then(data => {
+            this.setState(prevState => {
+                return {
+                    backdropImage: data.movie.backdrop_path,
+                    rating: data.movie.average_rating,
+                    releaseDate: data.movie.release_date,
+                    overview: data.movie.overview,
+                    genre: data.movie.genres,
+                    runtime: data.movie.runtime,
+                    wasClicked: !prevState.wasClicked
+                }
+            })
         })
     }
     returnHome = () => {
         this.state.goBack()
         this.setState(prevState => {
             return {
+                overview: '',
+                genre: [],
+                runtime: 0,
                 wasClicked: !prevState.wasClicked
             }
         })
     }
     render() {
-        // return (
-        // <div className="card" onClick={this.handleClick}>
-        //     <img src={this.state.posterImage} alt={this.state.title} />
-        // </div>
-        // )
         return (
             <div>
                 {this.state.wasClicked ?
@@ -59,6 +74,10 @@ class MovieCard extends React.Component {
                     <article className="details-container">
                         <h2 className="movie-title">{this.state.title}</h2>
                         <p className="movie-details">{`⭐️ ${this.state.rating.toFixed(1)} stars ⭐️`}</p>
+                        <p>Release Date: {this.state.releaseDate}</p>
+                        <p>Run Time: {this.state.runtime} minutes</p>
+                        <p>Genres: {(this.state.genre).join()}</p>
+                        <p>Description: {this.state.overview}</p>
                     </article>
                 </div>  
                 :        
