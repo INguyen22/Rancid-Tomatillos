7180a5c905ce295dcfbae0b9bf3b417b22ffbe3a
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2022-08-25 19:49:24 -0500

    add directory folders for components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import react from "react"
-import Banner from "./assets/retro-banner.png"
+import Banner from "../../assets/retro-banner.png"
 import "./Header.css"
 
 
--- src/components/MoviesContainer/MoviesContainer.js
+++ src/components/MoviesContainer/MoviesContainer.js
@@ -1,5 +1,5 @@
 import React from "react"
-import MovieCard from "./MovieCard"
+import MovieCard from "../MovieCard/MovieCard"
 import "./MoviesContainer.css"
 
 const MoviesContainer = ({ movies , showMovieDetails, goBack}) => {
